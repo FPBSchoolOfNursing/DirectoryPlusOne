073386f0ea565495c31bcbb75037f5267cb0226d
Ben Margevicius <bdm4@case.edu>
2016-10-05 12:13:02 -0400

    Added DB Init on Startup

--- src/DirectoryPlusOne/Startup.cs
+++ src/DirectoryPlusOne/Startup.cs
@@ -40,12 +40,14 @@ namespace DirectoryPlusOne
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, DirectoryContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            DirectoryInitilizer.Initialize(context);
         }
     }
 }
